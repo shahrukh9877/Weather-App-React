8023f1765c3854e3dee356ca51d0c5223fbbd14c
shahrukh9877 <shahrukh.ahmad574@gmail.com>
2023-06-21 21:46:01 +0530

    commit

--- src/Main.jsx
+++ src/Main.jsx
@@ -24,13 +24,39 @@ function Main() {
     .then((res)=> res.json())
     .then((res)=>setTemp(res.temp))
   },[input])
+
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const weeks = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  const date = new Date();
+  const todaydate = date.getDate();
+  const day = date.getDay();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  const Month = months[month];
+  const Week = weeks[day];
+
+
   return (
     
         <div className="main_container">
            <div className="child">
             <input type="text"  onChange={handleOnChange} placeholder='Search...'/>     
             <h1 className='center_text'>{input}</h1>
-            <h3 className='center_text'>Monday 20 Jun 2023</h3>
+            <h3 className='center_text'>{`${Week} ${todaydate} ${Month} ${year}`}</h3>
             <div className="temp_box">
               <h1 id='center_temp'>{temp || "Loading..."}&deg;C </h1>
             </div>
